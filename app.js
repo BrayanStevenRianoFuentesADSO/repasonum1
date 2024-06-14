69055974e686cde8495bf7a0677a45490762aa70
BrayanStevenRianoFuentesADSO <brayanstevenriano@gmail.com>
2024-06-14 16:52:57 -0500

    nodos

---
+++ app.js
@@ -0,0 +1,29 @@
+let win = window
+let dom=document
+
+let body = dom.body
+//nuevas formas
+let divs = dom.querySelectorAll("div")
+let div = dom.querySelector("div")
+let form=dom.forms
+
+let porid = dom.getElementById("documento")
+let poridnuevo = dom.querySelector("#documento")
+let porclase = dom.querySelectorAll(".container")
+let clase =dom.getElementsByClassName("input")
+// console.log(dom.images)
+let litems = dom.querySelectorAll("ul.list> li.item")
+
+let api = dom.getElementById("api")
+let apimayuscula=api.textContent.toUpperCase()
+
+api.innerHTML
+
+console.log(api)
+
+
+// const listaA = [...body.children]
+
+// body.children.map((x) => {
+//   console.log(x)
+// })
