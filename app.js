dab9ce27bd1d162f1a005c1230f767145ba373f2
BrayanStevenRianoFuentesADSO <brayanstevenriano@gmail.com>
2024-06-19 16:14:16 -0500

    crear nodos

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let win = window
 let dom=document
 
 let body = dom.body
+
 //nuevas formas
 let divs = dom.querySelectorAll("div")
 let div = dom.querySelector("div")
@@ -12,6 +13,7 @@ let poridnuevo = dom.querySelector("#documento")
 let porclase = dom.querySelectorAll(".container")
 let clase =dom.getElementsByClassName("input")
 // console.log(dom.images)
+
 let litems = dom.querySelectorAll("ul.list> li.item")
 
 let api = dom.getElementById("api")
@@ -19,11 +21,57 @@ let apimayuscula=api.textContent.toUpperCase()
 
 api.innerHTML
 
-console.log(api)
+// console.log(api)
+
+// ejercicio
+
+const padre1 = dom.getElementById("cards");
+
+// console.log(padre1.children)
+
+const nuevo = [...padre1.children]
 
 
+
+nuevo.map((a, i) => {
+  if (i % 2 != 0) {
+    a.classList.toggle("bg-white")
+    
+
+  }
+})
+
+const formulario = dom.getElementById("search");
+ 
+formulario.setAttribute("autocomplete", "on")
+
+const input1 = dom.querySelector(".input")
+// input1.setAttribute("placeholder", "")
+
+input1.removeAttribute("placeholder")
+input1.setAttribute("placeholder", "Search")
+// input1.setAttribute("search")
 // const listaA = [...body.children]
 
 // body.children.map((x) => {
 //   console.log(x)
 // })
+
+//CUSTOM PROPIERTIES CSS
+
+const html1 = dom.documentElement
+
+console.log(html1)
+
+
+const MineShaft = getComputedStyle(html1).getPropertyValue("--MineShaft")
+const white = getComputedStyle(html1).getPropertyValue("--white")
+// console.log(MineShaft)
+
+// html1.style.setProperty("--MineShaft", "#fff")
+// html1.style.setProperty("--white", "#000000")
+
+const parrafo = dom.querySelector("#elementos")
+// parrafo.textContent = " <b>hola</b> <br> hola</br> holaaaaaaaaaaaaaaaaaaaa";
+parrafo.innerHTML = " <b>hola</b> <br> hola</br> holaaaaaaaaaaaaaaaaaaaa";
+console.log(parrafo)
